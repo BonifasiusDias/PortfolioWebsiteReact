e4e2819154bc17b1f457e3fa8313cb1697ea3eb8
BonifasiusDias <bonifasiusdias9@gmail.com>
2021-09-25 22:40:15 +0700

    First commit react app

---
+++ src/Pages/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
