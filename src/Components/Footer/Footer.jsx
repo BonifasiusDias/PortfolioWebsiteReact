ab71a24c32e6070f229d29e8e8ff9cf6abf45b50
BonifasiusDias <bonifasiusdias9@gmail.com>
2021-10-29 23:26:02 +0700

    add Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center  w-full h-14 bg-black">
+      <h1 className="text-gray-400 my-auto">
+        Made with love by Bonifasius Dias
+      </h1>
+    </div>
+  );
 };
 
 export default Footer;
