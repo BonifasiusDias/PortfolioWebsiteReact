f1034e001f6854287aa8157af1dd748880190f24
BonifasiusDias <bonifasiusdias9@gmail.com>
2021-10-29 23:25:53 +0700

    Finishing contact with styling

--- src/Components/Section/Contact.jsx
+++ src/Components/Section/Contact.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { LocationMarker, Phone, Mail } from "react-hero-icon/solid";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BsFacebook, BsLinkedin, BsInstagram } from "react-icons/bs";
+
 const Contact = () => {
   return (
-    <div className="font-nunito h-screen">
+    <div className="font-nunito h-screen m-20">
       <div className="grid grid-cols-2">
-        <div className="m-10">
-          <div>Contact Me!</div>
-          <p>
+        <div className="my-10 ml-10 border-r-2">
+          <div className="text-xl font-semibold my-2">Contact Me!</div>
+          <p className="h-16">
             If you have any criticism and suggestions or maybe a new project,
             you can contact me from my number/email below! I will try my best to
             answer :D{" "}
@@ -43,41 +44,44 @@ const Contact = () => {
           </div>
         </div>
         <div className="m-10">
-          <div>Social Media</div>
-          <p>
+          <div className="text-xl font-semibold my-2">Social Media</div>
+          <p className="h-16">
             Don't forget to check my social media account below for any updates
             from me! And don't be shy to follow
           </p>
           <div>
             <div className="flex my-4">
-              <a href="https://www.linkedin.com/in/bonifasiusdias/">
-                <FontAwesomeIcon icon={["fab", "apple"]} />
+              <a
+                class="text-red-600"
+                href="https://www.linkedin.com/in/bonifasiusdias/"
+              >
+                <BsLinkedin className="h-10" />
               </a>
-              <div>
+              <div className="ml-4 flex flex-col">
                 <div>LinkedIn</div>
                 <div>bonifasiusdias</div>
               </div>
             </div>
             <div className="flex my-4">
               <a
+                class="text-red-600"
                 href="https://www.instagram.com/bonifasiusdias/"
-                class="fab fa-instagram"
               >
-                <FontAwesomeIcon icon={["fab", "apple"]} />
+                <BsInstagram className="h-10" />
               </a>
-              <div>
+              <div className="ml-4 flex flex-col">
                 <div>Instagram</div>
                 <div>bonifasiusdias</div>
               </div>
             </div>
             <div className="flex my-4">
               <a
+                class="text-red-600"
                 href="https://www.facebook.com/bonifasius.dias/"
-                class="fab fa-facebook-f"
               >
-                <FontAwesomeIcon icon={["fab", "apple"]} />
+                <BsFacebook className="h-10" />
               </a>
-              <div>
+              <div className="ml-4 flex flex-col">
                 <div>Facebook</div>
                 <div>bonifasiusdias</div>
               </div>
@@ -85,52 +89,61 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <div class="bawah">
-        <div class="text">Message me</div>
-        <form action="" method="POST">
-          <div class="fields">
-            <div class="field name">
+      <div class="ml-20">
+        <div class="text-lg font-bold">Message me!</div>
+        <form className="flex mb-4" action="" method="POST">
+          <div class="w-1/2 my-2">
+            <textarea
+              className="w-11/12 border rounded-lg py-1 px-2 h-36 "
+              placeholder="Message.."
+              id="message"
+              name="message"
+              required
+            ></textarea>
+          </div>
+          <div className="w-2/6">
+            <div>
+              <div>
+                <input
+                  className="w-full my-2 py-1 px-2 border rounded-lg "
+                  type="text"
+                  placeholder="Name"
+                  id="name"
+                  name="name"
+                  required
+                />
+              </div>
+            </div>
+            <div class="">
               <input
-                type="text"
-                placeholder="Name"
-                id="name"
-                name="name"
+                className="w-full my-2  py-1 px-2 border rounded-lg "
+                type="email"
+                placeholder="Email"
+                id="email"
+                name="email"
                 required
               />
             </div>
-            <div class="field textarea">
-              <textarea
-                cols="30"
-                rows="10"
-                placeholder="Message.."
-                id="message"
-                name="message"
+            <div class="">
+              <input
+                className="w-full my-2  py-1 px-2 border rounded-lg "
+                type="text"
+                placeholder="Subject"
+                id="subject"
+                name="subject"
                 required
-              ></textarea>
+              />
             </div>
           </div>
-          <div class="field email">
-            <input
-              type="email"
-              placeholder="Email"
-              id="email"
-              name="email"
-              required
-            />
-          </div>
-          <div class="field subject">
-            <input
-              type="text"
-              placeholder="Subject"
-              id="subject"
-              name="subject"
-              required
-            />
-          </div>
-          <div class="button-area">
-            <button type="submit">Send</button>
-          </div>
         </form>
+        <div>
+          <button
+            className="bg-red-600 text-white py-1 px-6 rounded-lg hover:text-red-600 border-2 border-red-600 hover:bg-white"
+            type="submit"
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
